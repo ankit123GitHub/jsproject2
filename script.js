f940afdb78fbe44665e12a1f770f9db6b2055d38
DHANDHALYA ANKIT <100505674+ankit123GitHub@users.noreply.github.com>
2023-11-27 10:53:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0 ; i < 6 ; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+};
+
+console.log(randomColor());
+
+let intervalID;
+const startChangingColor = function(){
+
+    intervalID = setInterval(changeBgcolor,1000);
+    
+    function changeBgcolor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+const stopChangingColor = function(){
+    clearInterval(intervalID);
+};
+
+document.querySelector("#start").addEventListener
+('click',startChangingColor);
+
+document.querySelector("#stop").addEventListener
+('click',stopChangingColor);
